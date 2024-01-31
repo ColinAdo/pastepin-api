88820293cfbe59dc0d2cab41e34903e44469f48f
ColinAdo <colinadodev30@gmail.com>
2024-01-31 13:43:19 +0300

    Add an endpoint for a root api

--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.models import User
+
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import reverse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Snippet
 from .permisssions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 
+
+@api_view
+def api_root(request, format=None):
+    return Response(
+        {
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippets-list', request=request, format=format)
+        }
+    )
     
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
